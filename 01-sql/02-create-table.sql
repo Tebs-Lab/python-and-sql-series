f86ec4813a116fb806d05de36f7fb6b7f70a9731
Tyler Bettilyon <teb@tebs-lab.com>
2022-04-06 10:32:51 -0600

    inital restructure and importing of lessons to drop the slideshow based content

---
+++ 01-sql/02-create-table.sql
@@ -0,0 +1,11 @@
+-- Create tables
+
+-- Datatypes
+
+-- Constraints (unique, null/not null, "there are many others")
+
+-- Default Values
+
+-- Alter existing table (not common, but sometimes needed)
+
+-- Drop tables
---
+++ 01-sql/04-basic-select.sql
@@ -0,0 +1,9 @@
+-- Select everything
+
+-- Specify columns
+
+-- Specify rows with where
+
+-- Limit number of results
+
+-- Sort the results with order by
